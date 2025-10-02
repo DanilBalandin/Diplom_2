a3c5920b9545ee04090e8cf5291838df371600dd
Danil <danilbalandin1@yandex.ru>
2025-10-02 19:58:32 +0300

    Создал класс-хранилище, для создания различных наборов аккаунтов

---
+++ src/test/java/testAccountData/TestAccountOrder.java
@@ -0,0 +1,60 @@
+package testAccountData;
+
+import accountProfile.Account;
+
+import java.util.UUID;
+
+
+//Планирую вынести создание тестовых-аккаунтов в отдельный "менеджер аккаунтов"
+
+public class TestAccountOrder {
+
+    //Создаем рандомный аккаунт
+    public static Account createRandomAccount() {
+        String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+        return new Account(
+                "user." + uniqueId + "@testmail.com",
+                "TestAccount_" + uniqueId,
+                "samplePassword@" + uniqueId
+        );
+    }
+
+    public static Account createAccountWithSpecificEmail(String email) {
+        String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+        return new Account(
+                email,
+                "TestAccount_" + uniqueId,
+                "samplePassword@" + uniqueId
+        );
+
+    }
+    //Без почты
+    public static Account createAccountWithoutEmail() {
+        String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+        return new Account(
+                null,
+                "TestAccount_" + uniqueId,
+                "samplePassword@" + uniqueId
+        );
+    }
+    //Без пароля
+    public static Account createAccountWithoutPassword() {
+        String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+        return new Account(
+                "user." + uniqueId + "@testmail.com",
+                "TestAccount_" + uniqueId,
+                null
+        );
+    }
+
+    //Без имени
+    public static Account createAccountWithoutName() {
+        String uniqueId = UUID.randomUUID().toString().substring(0, 8);
+        return new Account(
+                "user." + uniqueId + "@testmail.com",
+                null,
+                "samplePassword@" + uniqueId
+        );
+    }
+
+}
